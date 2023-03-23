186131c362b18d530320af0536747a95d3dc9fc1
david.sot <davidsoto_2@hotmail.com>
2023-03-23 14:51:58 -0300

    añadiendo mis primeros archivos en git

---
+++ script.js
@@ -0,0 +1,49 @@
+
+/* for(let i = 1; i <= 10; i = i + 1){
+    console.log("Hola mundo")
+}
+const listaDeProductos = ["Moto G", "pixel 3", "V 9", "iphone 10"]
+/* console.log(listaDeProductos) */
+/* for(let i = 0;  i <listaDeProductos.length; i = i + 1){
+    console.log("el valor de la variable i es")
+    console.log(listaDeProductos[i])
+
+} */ 
+/* var nombre = "pepe"
+if(true){
+    nombre = "lucas"
+    if(true){
+        let nombre = "maria"
+        console.log(nombre)
+    }
+    console.log(nombre)
+}
+console.log(nombre) */
+/* let nombre = "pepe"
+if(true){
+    let nombre = "lucas"
+    console.log(nombre)
+}
+console.log(nombre) */
+/* const notas1erSemestre = [8, 1, 3, 10, 7, 2]
+const notas2doSemestre = [8, 1, 3, 10, 7, 2] */
+
+
+
+/* let sumatoriaDeNotas = 0
+for(let i = 0; i < notas1erSemestre.length; i =i + 1){
+    sumatoriaDeNotas = sumatoriaDeNotas + notas1erSemestre[1]
+}
+let promedio = sumatoriaDeNotas / notas1erSemestre.length
+console.log("su promeio final es:" + promedio) */
+
+
+/* const saludar = (x) => {
+    console.log("Hola " + x)
+}
+saludar ("pepe")
+ */
+const saludar = (nombre, apellido) => {
+    return "Hola " + nombre + "" + apellido
+}
+saludar ("pepe")
